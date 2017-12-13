ef23cf10b57c86c4214bf98fb2b201e62a49e3f7
Szymon Przedwojski <szymon.przedwojski@bms.com.pl>
2017-12-13 16:41:41 +0100

    First working version
    Change-Id: I55cf3560295d1fcf5de64a1b0682bde11f91d893

---
+++ README.md
@@ -0,0 +1,17 @@
+## Trello Bot
+
+Script that automatically moves cards between lists in Trello.
+
+Add this script to your CI (e.g. Jenkins) to be run in various moments of your workflow.
+
+#### Usage
+
+1. Create `config.cfg` from `config.cfg-template`.
+2. Fill in the required information:
+    - TODO
+3. Configure the script to be run from CI
+    - TODO
+    
+#### Dependencies
+
+- JQ
--- trellify.sh
+++ trellify.sh
@@ -6,13 +6,13 @@ export PATH=$PATH:/opt
 card_id_short=$1
 target_list_name=$2
 
-board_id='58ca5654a51dfec0ffa91369'
-korekta_id='594cd61b9117fe35e10c3120'
-code_review_id='576d08916b4642cfaad30af7'
-verify_id='576d0896f6d3ed32ca2d15f2'
-token='9e72c78b57fd133772cb9967edebbcfe4c9a184bb6304ce8f6ab3a23b3a01a4e'
-key='ad3b3a870fe9443fc78044267e606046'
+if [ ! -f config.cfg ]; then
+    echo "ERROR: Config file config.cfg missing. Please create it from config.cfg-template. Aborting."
+    exit 0
+fi
+source config.cfg
 
+# TODO: Generalize list names
 if [ "$target_list_name" == "korekta" ]; then
 	target_list_id=$korekta_id
 elif [ "$target_list_name" == "verify" ]; then
